cd2cbaa784bcdccd31ccae1e97bc2d8b2cb2cf92
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-09 02:10:14 -0300

    UPDATE: requisito 2 requisito 2 implementado

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const memeInsert = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
 
-textInput.oninput = function textMeme() {
+function textMeme() {
   memeText.innerText = textInput.value;
-};
+}
+textInput.addEventListener('input', textMeme);
+
+function getImage() {
+  memeInsert.addEventListener('change', function putImage() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      const uploadedImage = reader.result;
+      memeImage.src = uploadedImage;
+    });
+    reader.readAsDataURL(this.files[0]);
+  });
+}
+
+getImage();
