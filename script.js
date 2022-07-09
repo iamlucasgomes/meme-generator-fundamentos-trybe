ecfca8f418c01b038f6db37d71d8763a704105b4
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-09 18:35:14 -0300

    UPDATE: requisito 6 requisito 6 implementado

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
+const memeContainer = document.querySelector('#meme-image-container');
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
+const carbon = document.querySelector('#carbon');
 
 function textMeme() {
   memeText.innerText = textInput.value;
 }
 textInput.addEventListener('input', textMeme);
 
-function getImage() {
+function setImage() {
   memeInsert.addEventListener('change', function putImage() {
     const reader = new FileReader();
     reader.addEventListener('load', () => {
@@ -19,4 +24,28 @@ function getImage() {
   });
 }
 
-getImage();
+setImage();
+
+function setFire() {
+  memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+}
+
+fire.addEventListener('click', setFire);
+
+function setWater() {
+  memeContainer.style.border = '5px double rgb(0, 0, 255)';
+}
+
+water.addEventListener('click', setWater);
+
+function setEarth() {
+  memeContainer.style.border = '6px groove rgb(0, 128, 0)';
+}
+
+earth.addEventListener('click', setEarth);
+
+function setCarbon() {
+  memeContainer.style.border = '1px solid black';
+}
+
+carbon.addEventListener('click', setCarbon);
