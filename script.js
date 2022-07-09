2c491d8cbea185c8934262d6946b00300ecae812
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-09 19:18:29 -0300

    UPDATE: requisito 7 requisito 7 implementado

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const fire = document.querySelector('#fire');
 const water = document.querySelector('#water');
 const earth = document.querySelector('#earth');
 const carbon = document.querySelector('#carbon');
+const images = document.querySelectorAll('#memes-container img');
 
 function textMeme() {
   memeText.innerText = textInput.value;
@@ -49,3 +50,13 @@ function setCarbon() {
 }
 
 carbon.addEventListener('click', setCarbon);
+
+function thumbImagesClick() {
+  for (let index = 0; index < images.length; index += 1) {
+    images[index].addEventListener('click', () => {
+      memeImage.src = images[index].src;
+    });
+  }
+}
+
+thumbImagesClick();
